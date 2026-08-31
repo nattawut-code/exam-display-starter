cb8158bc015943fd34642ff3e5df746ac9810a86
nattawut-code <nattawut@benjama.ac.th>
2026-08-31 09:10:17 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // ใส่ค่าจาก Supabase Project Settings > API
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://sqpdfqbdtdcrhbixsmnh.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_X-XzZyGXBm9LIKgGwirV0Q_28d0IfXb";
 
 // ชื่อ bucket ที่สร้างใน Supabase Storage
 const STORAGE_BUCKET = "exam-images";
